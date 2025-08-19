bcdd12307105f95c3c7656a8883c316bf9dfdfbc
Heloisa Lacerda <125582181+loisaaz@users.noreply.github.com>
2025-08-18 23:10:34 -0300

    iniciado o ex 2

---
+++ ex2.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int num, cont, max = -1000, min = 999;
+    while (num != 0)
+    {
+        printf("tecle um numero: ");
+        scanf("%d", &num);
+        cont++;
+    }
+    
+    printf("foram digitados %d números", cont);
+    //printf
+    return 0;
+}
