34277d73764f3f7b378a261224564cd7c00126c1
Heloisa L. Marinho <125582181+loisaaz@users.noreply.github.com>
2025-08-19 16:14:12 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,14 +1,27 @@
 int main()
 {
-    int num, cont, max = -1000, min = 999;
+	int num, cont = 0, max = 0, min = 999999;
+	
+	printf("\n\ttecle um numero: ");
+    scanf("%d", &num);
+	
     while (num != 0)
     {
-        printf("tecle um numero: ");
-        scanf("%d", &num);
         cont++;
+        
+        printf("\n\ttecle um numero: ");
+        scanf("%d", &num);
+        
+        if (num < min)
+            min = num;
+            
+        if (num > max)
+            max = num;
+        
     }
+
+    printf("\n\tforam digitados %d números", cont);
+    printf("\n\to maior numero foi: %d\n\t e o menor foi: %d", max, min);
     
-    printf("foram digitados %d números", cont);
-    //printf
-    return 0;
+	return 0;
 }
